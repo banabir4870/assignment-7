2141e4588d911b4cbebb14530547e9c76eaf2d40
banabir4870 <banabir4870@gmail.com>
2026-04-17 13:21:47 +0600

    added friend details, quicklinks and responsiveness

--- src/app/friends/[id]/page.jsx
+++ src/app/friends/[id]/page.jsx
@@ -1,7 +1,9 @@
 import Image from 'next/image';
 import React from 'react';
+import { BsCameraVideo } from 'react-icons/bs';
 import { HiOutlineBellSnooze } from 'react-icons/hi2';
-import { IoArchiveOutline } from 'react-icons/io5';
+import { IoArchiveOutline, IoChatboxEllipsesOutline } from 'react-icons/io5';
+import { LuPhoneCall } from 'react-icons/lu';
 import { MdOutlineDelete } from 'react-icons/md';
 
 const FriendsDetail = async ({ params }) => {
@@ -11,7 +13,7 @@ const FriendsDetail = async ({ params }) => {
     const expectedFriend = allFriends.find(friends => friends.id == id);
 
     return (
-        <div className='w-9/12 mx-auto grid grid-cols-3 my-20 gap-4'>
+        <div className='w-9/12 mx-auto grid lg:grid-cols-3 my-20 gap-4'>
             {/* left side */}
             <div className='col-span-1'>
                 <div className="card bg-base-100 shadow-sm">
@@ -34,8 +36,8 @@ const FriendsDetail = async ({ params }) => {
                 </div>
             </div>
             {/* right side */}
-            <div className='col-span-2'>
-                <div className='grid grid-cols-3 gap-6'>
+            <div className='col-span-2 space-y-6'>
+                <div className='grid lg:grid-cols-3 gap-6'>
                     <div className="card bg-base-100 shadow-sm">
                         <div className="card-body text-center">
                             <h2 className="text-center text-3xl font-semibold text-[#244D3F]">{expectedFriend.days_since_contact}</h2>
@@ -58,6 +60,31 @@ const FriendsDetail = async ({ params }) => {
                                 })}
                             </h2>
                             <p className='text-xl text-[#64748B]'>Next Due</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="card bg-base-100 shadow-sm">
+                        <div className="card-body">
+                            <div className='flex justify-between items-center'>
+                                <h2 className="text-xl font-semibold text-[#244D3F]">Relationship Goal</h2>
+                                <button className='btn'>Edit</button>
+                            </div>
+                            <p className='text-lg text-[#64748B]'>Connect every <span className='text-[#1F2937] font-bold'>{expectedFriend.goal} days</span></p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="card bg-base-100 shadow-sm">
+                        <div className="card-body">
+                            <div className='flex justify-between items-center'>
+                                <h2 className="text-xl font-semibold text-[#244D3F]">Quick Check-In</h2>
+                            </div>
+                            <div className='grid grid-cols-3 gap-4'>
+                                <button className='btn h-auto w-full flex flex-col items-center text-lg justify-center p-4'><LuPhoneCall className='text-2xl' />Call</button>
+                                <button className='btn h-auto w-full flex flex-col items-center text-lg justify-center p-4'><IoChatboxEllipsesOutline className='text-2xl' />Text</button>
+                                <button className='btn h-auto w-full flex flex-col items-center text-lg justify-center p-4'><BsCameraVideo className='text-2xl' />Video</button>
+                            </div>
                         </div>
                     </div>
                 </div>
